efae1503a0d6f9c0b19918e320938dabea8ced04
Acharya amit <acharyaa619@gmail.com>
2020-06-03 09:46:20 +0545

    first commit

---
+++ var.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main(){
+        var a int = 5
+        var b float32 = 4.99
+        const pi float64 = 3.1415
+        var (
+                c= 8
+                d= 9
+        )
+        x,y := 14,15
+        fmt.Println("a=",a)
+        fmt.Println("b=",b)
+        fmt.Println("pi=",pi)
+        fmt.Println("c=",c)
+        fmt.Println("d=",d)
+        fmt.Println("x,y=",x,",",y)
+}
